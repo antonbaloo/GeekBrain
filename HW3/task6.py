081004945f87df46d653fea17a51b8064479d987
antonbaloo <flomaIZR@gmail.com>
2021-11-02 23:06:47 +0200

    hw3

--- HW3/task6.py
+++ HW3/task6.py
@@ -5,3 +5,21 @@
 # Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
 # Необходимо использовать написанную ранее функцию int_func().
 
+def int_funk(a):
+    b = a.title()
+    return b
+
+
+def int_funk_2(words):
+    upper_list = []
+    list_words = words.split(' ')
+    for _ in list_words:
+        upper = int_funk(_)
+        upper_list.append(upper)
+    new_words = ' '.join(upper_list)
+    print(new_words)
+
+
+words = input('ВВедите слова: ')
+
+int_funk_2(words)
