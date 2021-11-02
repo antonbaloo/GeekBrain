f56581c2c3eb3902d70bf1d5ff930a0c61ff3811
antonbaloo <flomaIZR@gmail.com>
2021-11-02 22:45:52 +0200

    lesson 5

--- Test.py
+++ Test.py
@@ -1,3 +1,24 @@
-f = open('secret.txt', 'w')
-f.write('TOP SECRET')
-f.close()
+# with open('base.txt', 'a') as f:
+#     f.write('HI broooo')
+#
+# with open('base.txt', 'rb') as f:
+#     print(f.read())
+
+#
+# with open('data.txt', 'r') as f:
+#     print(f.tell())
+#     print(f.read())
+#     print(f.tell())
+#     print('-' * 50)
+#     f.seek(30)
+#     print(f.tell())
+#     print(f.read())
+#     print(f.tell())
+
+# with open('data.txt', 'r') as f:
+#     data = f.read()
+#     print(len(data), f.tell())
+#     f.seek(57)
+#     print(f.read())
+
+
