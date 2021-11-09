716f31a76ae34a2dd8b98a5bab0708ff9083b2b5
antonbaloo <flomaIZR@gmail.com>
2021-11-09 22:45:17 +0200

    HW6

---
+++ HW6/Task3.py
@@ -0,0 +1,30 @@
+# Реализовать базовый класс Worker (работник),
+# в котором определить атрибуты: name, surname, position (должность),income (доход).
+# Последний атрибут должен быть защищенным и ссылаться на словарь,
+# содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+# Создать класс Position (должность) на базе класса Worker.
+# В классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+# и дохода с учетом премии (get_total_income). Проверить работу примера на реальных данных
+# (создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wage': wage, 'bonus': bonus}
+
+
+class Position(Worker):
+
+    def get_full_name(self):
+        print(f'{self.name} {self.surname}')
+
+    def get_total_income(self):
+        print(sum(self._income.values()))
+
+
+man_1 = Position('Paul', 'Konopel', 'worker', 10000, 2000)
+man_1.get_full_name()
+man_1.get_total_income()
+
